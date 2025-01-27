847c1329bcaf39148086a75452aeae14ee53a92a
Koushil C <koushil463@gmail.com>
2025-01-27 21:56:16 +0530

    resolved minor bug

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -5,7 +5,7 @@ const { validationResult } = require('express-validator');
 module.exports.registerUser = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.errors.array() });
+        return res.status(400).json({ errors: errors.array() });
     }
     const { fullname, email, password } = req.body;
     const hashPassword = await userModel.hashpassword(password);
